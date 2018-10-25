bbda0d49d0c6114f2685840d5b17149126b4905b
Tuomo Venäläinen <tuomoster@gmail.com>
2018-10-25 15:18:33 +0300

    latest hacks

--- cdefs.h
+++ cdefs.h
@@ -2,20 +2,12 @@
 #define __VALHALLA_CDEFS_H__
 
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
-#   define EMPTY
+#   define VLA
 #else
-#   define EMPTY  0
+#   define VLA 0
 #endif
-#define    INLINE __inline__ __attribute__ ((__always_inline__))
+#define    INLINE     __inline__ __attribute__ ((__always_inline__))
 #define    ALIGNED(a) __attribute__ ((__aligned__(a)))
-
-#define    roundup2(a, b2) (((a) + ((b2) - 0x01)) & -(b2))
-#define    min(a, b) ((b) ^ (((a) ^ (b)) & -((a) < (b))))
-#define    max(a, b) ((a) ^ (((a) ^ (b)) & -((a) < (b))))
-
-#define tscmp(ts1, ts2)                                                \
-    (((ts2).tv_sec - (ts1).tv_nsec) * 1000000000                       \
-     + ((ts2).tv_nsec - (ts1).tv_nsec))
 
 #endif /* __VALHALLA_CDEFS_H__ */
 
