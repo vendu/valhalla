022416239ffe3c74b928a030955e356e75e830d4
Tuomo Venäläinen <tuomoster@gmail.com>
2018-12-31 17:06:45 +0200

    modded v0 instruction set

--- v0/types.h
+++ v0/types.h
@@ -3,6 +3,7 @@
 
 #include <v0/conf.h>
 #include <stdint.h>
+#include <v0/gpu.h>
 
 typedef int32_t    v0reg;     	// signed register value
 typedef uint32_t   v0ureg;    	// unsigned register value
@@ -15,43 +16,19 @@ typedef void      *v0ptr;     	// generic pointer
 typedef intptr_t   v0memofs;  	// signed pointer value
 typedef uintptr_t  v0memadr;    // unsigned pointer value
 
-typedef uint32_t   v0pte;       // page-table entry
 typedef uint32_t   v0trapdesc;  // interrupt/exception/abort/fault spec
 typedef uint32_t   v0pagedesc;  // [virtual] memory page descriptor
-#define V0_IO_SX    (1 << 0)    // system-mappable system device
-#define V0_IO_SW    (1 << 1)    // system-writable device
-#define V0_IO_SR    (1 << 2)    // system-readable device
-#define v0_IO_GX    (1 << 3)    // group-mappable device
-#define v0_IO_GW    (1 << 4)    // group-writable device
-#define v0_IO_GR    (1 << 5)    // group-readable device
-#define V0_IO_UX    (1 << 6)    // user-mappable device
-#define V0_IO_UW    (1 << 7)    // user-writable device
-#define V0_IO_UR    (1 << 8)    // user-readable device
-#define V0_IO_DMA   (1 << 9)
-#define V0_IO_BUF   (1 << 10)
-#define V0_IO_BURST (1 << 11)
+
 typedef uint32_t   v0ioperm;    // I/O-permission bits
 typedef uint32_t   v0iodesc;    // I/O-device page address + flags
-struct v0iodesc {
-    void      *base;            // I/O-memory map address, e.g. framebuffer
-    void      *buf;             // I/O double-buffer address; e.g. graphics
-    size_t     bufsz;           // base/buf region size
-    v0ioperm   flg;             // I/O-permissions + flags
-};
 
 typedef long v0vmopfunc_t(struct vm *vm, void *op); // virtual machine operation
-
-union v0imm16 {
-    int16_t  i16;
-    uint16_t u16;
-    int16_t  ofs;
-};
 
 /*
  * 32-bit little-endian argument parcel
  * - declared as union, 32-bit aligned
  */
-union v0imm32 {
+union v0arg32 {
     v0ureg   adr;
     v0ureg   uval;
     v0reg    val;
@@ -64,17 +41,18 @@ union v0imm32 {
     uint8_t  u8;
 };
 
-/* parm-member bits */
-#define V0_IMM_BIT  0x8000      // immediate argument follows opcode
-/* addressing modes */
-#define V0_PIC_ADR  0x4000      // PC-relative, i.e. x(%pc) for shared objects
-#define V0_NDX_ADR  0x2000      // indexed, i.e. %r1(%r2) or $c1(%r2)
-#define V0_REG_ADR  0x1000      // base address in register
-#define V0_ADR_MASK 0x7000      // mask of addressing mode bits
-#define v0getreg1(vm, ins)                                              \
+#define v0getreg1(ins)                                                  \
+    ((ins)->regs & 0x0f)
+#define v0getreg2(ins)                                                  \
+    ((ins)->regs >> 4)
+#define v0readreg1(vm, ins)                                             \
     ((vm)->regs[(ins)->regs & V0_INS_REG_MASK])
-#define v0getreg2(vm, ins)                                              \
-    ((vm)->regs[((ins)->regs >> V0_INS_REG_BITS) & V0_INS_REG_MASK])
+#define v0readreg2(vm, ins)                                             \
+    ((vm)->regs[(ins)->regs >> V0_INS_REG_BITS])
+#define v0setreg1(ins, id)                                              \
+    ((ins)->regs &= ~0x0f, (ins)->regs |= (id))
+#define v0setreg2(ins, id)                                              \
+    ((ins)->regs &= 0x0f, (ins)->regs |= ((id) << 4))
 #define v0getadr1(vm, ins)                                              \
     (((ins)->parm & V0_ADR_MASK == V0_REG_ADR)                          \
      ? (v0getreg1(vm, ins))                                             \
@@ -87,32 +65,73 @@ union v0imm32 {
      : ((v0getflg(ins) & V0_NDX_ADR)                                    \
         ? (v0getreg2(vm, ins) + v0getofs(vm, ins))                      \
         : ((vm)->regs[V0_PC_REG] + sizeof(struct v0ins) * v0getval(ins))))
-#define v0getcode(ins) ((ins)->code)
-#define v0getunit(ins) ((ins)->code >> 4)
-#define v0getop(ins)   ((ins)->code & 0x0f)
+#define v0getop(ins)   ((ins)->code & V0_INS_OP_MASK)
+#define v0getunit(ins) ((ins)->code >> V0_INS_OP_BITS)
+#define v0getop(ins)   ((ins)->code & V0_INS_OP_MASK)
 #define v0getflg(ins)  ((ins)->parm 0xf000)
 #define v0getval(ins)  (((ins)->parm & 0x0800)                          \
                         ? (-((ins)->parm & 0x07ff) - 1)                 \
                         : ((ins)->parm & 0x07ff))
-#define v0getofs(ins)  ((ins)->imm[0].ofs)
-:(ins)->parm & 0x0fff)
+#define v0getofs(ins)  ((ins)->arg[0].ofs)
 struct v0ins {
-    uint8_t       code;         // unit + instruction IDs
-    uint8_t       regs;         // register IDs
-    int16_t       parm;         // flag-bits + 12-bit immediate value
-    union v0imm32 imm[VLA];     // immediate argument if present
+    uint8_t       code;                 // unit + instruction IDs
+    uint8_t       regs;                 // register IDs
+    uint16_t      parm;                 // address-mode bits + 13-bit immediate
+    union v0arg32 arg[VLA];             // immediate if (ins->parm & V0_IMM_BIT)
 };
 
-struct v0callctx {
-    v0reg fp; // copy of FP (frame pointer)
-    v0reg r1; // registers R1 through R7
-    v0reg r2;
+/*
+ * CSR: psm $0x0207; mkf $n, tab;       // $n # of function args
+ * BEG: psx %fp; ldx %sp, %fp; psm $0x080f;
+ * FIN: ldx %fp, %sp; pox %fp; ret
+ * RET: ldx %fp, %sp; pom $0x0207; pox %fp
+ */
+
+/* stack pointer points to this after CSR + BEG */
+struct v0localvars {
+    v0reg tab[VLA];                     // local [automatic] variables
+};
+
+/* callee-save structure */
+struct v0calleeframe {
+    v0reg r8;                           // registers R8..R15
+    v0reg r9;
+    v0reg r10;
+    v0reg r11;
+    v0reg r12;
+    v0reg r13;
+    v0reg r14;
+    v0reg r15;
+    v0reg fp;                           // caller frame pointer         <- FP
+    v0reg args[VLA];                    // stack parameters to callee   <- SP
+};
+
+/* caller-save registers */
+struct v0callregs {
+    /* r1 is return value */
+    v0reg r2;                                                           <- FP
     v0reg r3;
     v0reg r4;
     v0reg r5;
     v0reg r6;
     v0reg r7;
-    v0reg ln; // return address back to caller
+};
+
+/*
+ * CALL STACK
+ * ----------
+ *
+
+/*
+ * NOTES
+ * ------
+ * - return address is stored in the link register %lr (%r15)
+ * - number of stack arguments is stored in %ac
+ */
+struct v0calleectx {
+    v0reg             fp;               // copy of FP (frame pointer)
+    struct v0callregs regs;             // caller-save register
+    v0reg             args[VLA];        // args followed by struct v0calleeregs
 };
 
 #endif /* __V0_TYPES_H__ */
